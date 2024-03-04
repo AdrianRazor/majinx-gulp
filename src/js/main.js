b2ff03057dc5b3936b97f97e4bcd87699d8c3294
Andy Razor <adrianrazor@ukr.net>
2024-03-04 17:33:26 +0200

    added modal

--- src/js/main.js
+++ src/js/main.js
@@ -13,5 +13,29 @@ document.addEventListener("DOMContentLoaded", function (event) {
     });
   }
 
+  // Modal contact
+  const contactBtn = document.querySelectorAll(".openModal");
+  const modal = document.querySelector(".modal");
+
+  if (contactBtn.length && modal) {
+    contactBtn.forEach((el) => {
+      el.addEventListener("click", () => {
+        modal.classList.add("open");
+      });
+    });
+
+    const closeBtn = modal.querySelector(".modal__close");
+
+    closeBtn.addEventListener("click", () => {
+      modal.classList.remove("open");
+    });
+
+    window.addEventListener("click", (event) => {
+      if (event.target.classList.contains("modal")) {
+        modal.classList.remove("open");
+      }
+    });
+  }
+
   console.log("DOM fully loaded and parsed");
 });
