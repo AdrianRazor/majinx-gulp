70db2ab3b80e3705080cd709a54d4e19633f5b7a
Andy Razor <adrianrazor@ukr.net>
2024-03-04 10:36:04 +0200

    added header

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,16 @@
 document.addEventListener("DOMContentLoaded", function (event) {
+  const root = document.documentElement;
 
+  const header = document.querySelector(".header");
+  const burger = document.querySelector(".burger");
 
+  if (header && burger) {
+    burger.addEventListener("click", () => {
+      header.classList.toggle("open");
+      burger.classList.toggle("open");
+      root.classList.toggle("lock");
+    });
+  }
 
-  
   console.log("DOM fully loaded and parsed");
 });
